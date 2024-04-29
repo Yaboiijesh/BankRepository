dd15e1d03e151fa4d5acb6dfe222850ecaf8b15b
Yaboiijesh <swainbrothers@outlook.com>
2024-04-28 17:11:14 -0700

    new repo

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import mysql.connector
+
+connection= connection = mysql.connector.connect(
+    user= 'root',
+    database= 'sample',
+    password= '2gd4fart'
+)
+
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM bank_info")
+cursor.execute(testQuery)
+
+
+for row in cursor:
+    print(row)
+
+cursor.close()
+connection.close()
